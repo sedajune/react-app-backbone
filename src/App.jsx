ef689fa7546997ca1cfb9d485640d637c0c2cf95
Silke Groenke <silkigr@gmail.com>
2022-01-20 20:10:56 +0100

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,53 @@
 import React from "react";
-import Button from "../src/components/Button";
+import { useCallback, useState, useEffect } from "react";
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello test</h1>
-      <Button>Click me!</Button>
-    </div>
-  );
+const Code = ({ code }) => (
+	<pre>
+		<code>{JSON.stringify(code, null, 4)}</code>
+	</pre>
+);
+
+const useLocalStorage = (key, initialValue) => {
+	const [value, setValue] = useState(JSON.parse(window.localStorage.getItem(key)));
+
+	useEffect(() => {
+		const storedValue = JSON.parse(window.localStorage.getItem(key));
+		console.log(storedValue);
+		if (storedValue !== null) {
+			setValue(storedValue);
+		}
+	}, [key]);
+
+	//useEffect( () => {
+	//window.localStorage.setItem(key, JSON.stringify(value));
+	//}, [key, value]);
+
+	const storeValue = useCallback(
+		newValue => {
+			window.localStorage.setItem(key, JSON.stringify(newValue));
+			setValue(newValue);
+		},
+		[key]
+	);
+
+	return [value, storeValue];
 };
 
+const App = () => {
+	const [checked, setChecked] = useLocalStorage("secret", false);
+	useEffect(() => {
+		console.log("triggered");
+	}, [setChecked]);
+	return (
+		<div>
+			<input
+				type="checkbox"
+				checked={checked}
+				onChange={() => {
+					setChecked(!checked);
+				}}
+			/>
+		</div>
+	);
+};
 export default App;
